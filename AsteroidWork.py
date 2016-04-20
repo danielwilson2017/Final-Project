a812c5c6ea4a9d777fd3ddb3a33b81c608d80072
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-04-20 13:58:53 -0400

    Updated from Brython Server: 4/20/2016 1:58:54 PM

--- AsteroidWork.py
+++ AsteroidWork.py
@@ -11,20 +11,21 @@ SCREEN_HEIGHT = 700
 
 lvl = int(input("What is your level? "))
 #randomizing the spawn point of the asteroid 
-h=1
-
-while h==1:
-    b=random.randint(1,1000)
-    c=random.randint(1,700)
-    if b<=1250 and c<=5:
-        h=0
-        print(b,c)
-    elif b<=5 and c<=700:
-        h=0
-        print(b,c)
-    else:
-        h=1
-        #print(b,c)
+nums=lvl
+while 
+    h=1
+    while h==1:
+        b=random.randint(1,1000)
+        c=random.randint(1,700)
+        if b<=1250 and c<=5:
+            h=0
+            print(b,c)
+        elif b<=5 and c<=700:
+            h=0
+            print(b,c)
+        else:
+            h=1
+            #print(b,c)
 
 class Asteroid(Sprite):
     
