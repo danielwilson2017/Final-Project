af3a19a52eccf90d4877047716f25110077c006f
danielwilson2017 <danielwilson2017@hanovernorwichschools.org>
2016-04-26 14:31:48 -0400

    Updated from Brython Server: 4/26/2016 2:31:49 PM

--- Randomizing.py
+++ Randomizing.py
@@ -14,16 +14,25 @@ lvl = int(input("What is your level? "))
 #randomizing the spawn point of the asteroid 
 nums=lvl
 
+
 h=1
 while h==1:
     b=random.randint(1,1000)
     c=random.randint(1,700)
-    if b<=1250 and c<=5:
+    d=random.randint(1,1000)
+    e=random.randint(1,700)
+    if b<=1250 and d<=1250 and e<=5 and c<=5:
         h=0
-        print(b,c)
+        f=list(zip(b,c))
+        g=list(zip(d,e))
+        m=random.randint(1,5)
+        while m<=3:
+            j=(
+'''
     elif b<=5 and c<=700:
         h=0
         print(b,c)
+'''
     else:
         h=1
         #print(b,c)
